9436e499c55a1c68354f0d3d68fac21d52696752
NoahBrezet <143404345+NoahBrezet@users.noreply.github.com>
2024-06-14 11:16:18 +0200

    tried show/hide img

--- login-page.js
+++ login-page.js
@@ -2,13 +2,19 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
+function showImage() {
+    var img = document.getElementById("hiddenImage");
+    img.style.visibility = "visible";
+}
+
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev") {
+    if (username === "userz" && password === "infinite22") {
         alert("You have successfully logged in.");
+        showImage
         location.reload();
     } else {
         loginErrorMsg.style.opacity = 1;
